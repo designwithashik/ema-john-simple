6a7fe7646f079fe9beaa0649dbdeb9879f635b47
Ashik <designwithashik@gmail.com>
2023-04-05 14:13:40 +0600

    Routes Added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import './Header.css'
 import logo from '../../images/Logo.svg'
+import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <nav className='header'>
             <img src={logo} alt="logo" />
             <div>
-            <a href="shop">shop</a>
-            <a href="/order">Order</a>
-            <a href="/Inventory">Inventory</a>
-            <a href="/Login">Login</a>
+            <Link to="/">shop</Link>
+            <Link to="/orders">Order</Link>
+            <Link to="/Inventory">Inventory</Link>
+            <Link to="/login">Login</Link>
             </div>
         </nav>
     );
